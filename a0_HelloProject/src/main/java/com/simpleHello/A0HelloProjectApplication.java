3c153986492876490a58be22815e8dcef0d1e557
Dipak Ugale <ugale.deepak3010@gmail.com>
2025-05-28 14:55:25 +0530

    SImple Hello Project added.

---
+++ a0_HelloProject/src/main/java/com/simpleHello/A0HelloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.simpleHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0HelloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A0HelloProjectApplication.class, args);
+	}
+
+}
